f2822b807098e17397cbd47e4eaefc66c7412d55
ahmadNazl <mr.nazlabadi@gmail.com>
2024-01-08 23:49:26 +0330

    instal laravel breeze, admin factory and seeder costumized

---
+++ lang/fa/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'این مشخصات نا معتبر است',
+    'password' => 'رمز عبور وارد شده نامعتبر است',
+    'throttle' => 'تلاش های زیادی برای ورود به سیستم انجام شده لطفاً در چند ثانیه دیگر دوباره امتحان کنید',
+
+];
